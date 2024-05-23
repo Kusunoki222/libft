2b82f211d7bc0ce0de70f8f72f7671d5212a0b3f
Kotaro Kusunoki <kkusunok@c2r8s1.42tokyo.jp>
2024-05-23 19:56:16 +0900

    bonus is far from here

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,42 +6,73 @@
 /*   By: kkusunok <kkusunok@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 11:16:28 by kkusunok          #+#    #+#             */
-/*   Updated: 2024/05/21 17:52:01 by kkusunok         ###   ########.fr       */
+/*   Updated: 2024/05/23 18:47:15 by kkusunok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isspace(char c)
+static long	overflow_check(int sign, const char *str)
 {
-	return ((c >= 9 && c <= 13) || c == 32);
+	long	temp;
+	long	value;
+
+	value = 0;
+	while (ft_isdigit(*str))
+	{
+		temp = value * 10 + *str - '0';
+		if (sign == 1 && !(temp >= value))
+			return (LONG_MAX);
+		else if (sign == -1 && !(temp >= value))
+			return (LONG_MIN);
+		value = temp;
+		str++;
+	}
+	return (value);
 }
 
 int	ft_atoi(const char *str)
 {
-	unsigned int	result;
-	int				sign;
-	unsigned int	digit;
+	long	value;
+	int		sign;
 
-	result = 0;
 	sign = 1;
-	while (ft_isspace(*str))
+	value = 0;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
+	if (*str == '-')
+		sign = -1;
 	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
 		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		digit = *str - '0';
-		if (sign == 1 && ((int)result > (INT_MAX - (int)digit) / 10))
-			return (INT_MAX);
-		if (sign == -1 && ((int)result > (INT_MAX - (int)digit + 1) / 10))
-			return (INT_MIN);
-		result = result * 10 + digit;
-		str++;
-	}
-	return (result * sign);
+	value = overflow_check(sign, str);
+	return ((int)(value * sign));
 }
+
+// int	ft_atoi(const char *str)
+// {
+// 	unsigned int	result;
+// 	int				sign;
+// 	unsigned int	digit;
+
+// 	result = 0;
+// 	sign = 1;
+// 	while (*str == 32 || (*str >= 9 && *str <= 13))
+// 		str++;
+// 	if (*str == '-' || *str == '+')
+// 	{
+// 		if (*str == '-')
+// 			sign = -1;
+// 		str++;
+// 	}
+// 	while (*str >= '0' && *str <= '9')
+// 	{
+// 		digit = *str - '0';
+// 		if (sign == 1 && ((int)result > (INT_MAX - (int)digit) / 10))
+// 			return (INT_MAX);
+// 		if (sign == -1 && ((int)result > (INT_MAX - (int)digit + 1) / 10))
+// 			return (INT_MIN);
+// 		result = result * 10 + digit;
+// 		str++;
+// 	}
+// 	return (result * sign);
+// }
