1fc5eacddac0ca93f5e297b5e1ba5bdc4954c8e6
Kotaro Kusunoki <kkusunok@c1r2s8.42tokyo.jp>
2024-05-31 20:19:30 +0900

    part2 fin

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: kkusunok <kkusunok@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 09:58:56 by kkusunok          #+#    #+#             */
-/*   Updated: 2024/05/23 14:17:59 by kkusunok         ###   ########.fr       */
+/*   Updated: 2024/05/31 20:16:13 by kkusunok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,28 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
+	size_t	orglen;
 	size_t	sublen;
 	size_t	i;
 
 	if (!s)
 		return (NULL);
-	else if (ft_strlen(s) <= start || len == 0)
+	orglen = ft_strlen(s);
+	if (orglen <= start || len == 0)
 		sublen = 0;
-	else if (ft_strlen(s) > start + len)
+	else if (orglen > start + len)
 		sublen = len;
 	else
-		sublen = ft_strlen(s) - start;
+		sublen = orglen - start;
 	substr = (char *)malloc((sublen + 1) * sizeof(char));
 	if (!substr)
 		return (NULL);
 	i = 0;
 	while (i < sublen)
+	{
 		substr[i] = s[start + i];
 		i++;
+	}
 	substr[sublen] = '\0';
 	return (substr);
 }
